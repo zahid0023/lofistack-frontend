dcfcea11891af245bede5337a8eba77e01287c04
ZBM Zubayer <zbmzubayer@gmail.com>
2026-02-11 00:58:45 +0600

    chore: lint-staged config

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,3 +1,5 @@
 export default {
-  "**/*.{js,ts,jsx,tsx,md,mdx,json}": () => ["pnpm biome check --write --no-errors-on-unmatched"],
+  "**/*.{js,ts,jsx,tsx,md,mdx,json}": () => [
+    "pnpm biome check --write --no-errors-on-unmatched",
+  ],
 };
